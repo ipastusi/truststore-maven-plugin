8ac1976586cd379d24adf62616a24f57176b8bc2
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2020-12-20 10:44:59 +0100

    Fix IT

--- src/it/basic/bin/keytool_asserts.sh
+++ src/it/basic/bin/keytool_asserts.sh
@@ -5,7 +5,7 @@ EXPECTED_OUTPUT=(
   "Your keystore contains 8 entries"
   "89:3c:56:46:8f:7b:4c:24:f8:03:17:d2:70:05:6a:5b"
   "7d:5b:51:26:b4:76:ba:11:db:74:16:0b:bc:53:0d:a7"
-  "1f:d6:d3:0f:ca:3c:a5:1a:81:bb:c6:40:e3:50:32d"
+  "01:fd:6d:30:fc:a3:ca:51:a8:1b:bc:64:0e:35:03:2d"
 )
 
 for entry in "${EXPECTED_OUTPUT[@]}"; do
