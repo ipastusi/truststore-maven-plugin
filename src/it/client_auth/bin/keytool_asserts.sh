5f1e94db12304007eddbfed4a8977fb447655f6d
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2021-01-07 20:45:20 +0100

    grep ignore case

--- src/it/client_auth/bin/keytool_asserts.sh
+++ src/it/client_auth/bin/keytool_asserts.sh
@@ -9,7 +9,7 @@ EXPECTED_OUTPUT=(
 RESULT=$(keytool -list -keystore target/truststore.jks -storepass changeit)
 
 for ENTRY in "${EXPECTED_OUTPUT[@]}"; do
-  FOUND=$(echo "${RESULT}" | grep -q "${ENTRY}" && echo 1 || echo 0)
+  FOUND=$(echo "${RESULT}" | grep -iq "${ENTRY}" && echo 1 || echo 0)
 
   if [ "${FOUND}" != "1" ]; then
     echo "Expected output '${ENTRY}' not found"
