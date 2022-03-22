0ddcf579d05fddd951d086cae2b76d40f04e9eeb
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2022-03-22 16:55:52 +0000

    Add unit tests

---
+++ src/test/java/uk/co/automatictester/truststore/maven/plugin/keystore/KeyStoreFactoryTest.java
@@ -0,0 +1,30 @@
+package uk.co.automatictester.truststore.maven.plugin.keystore;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import uk.co.automatictester.truststore.maven.plugin.truststore.TruststoreFormat;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.co.automatictester.truststore.maven.plugin.truststore.TruststoreFormat.*;
+
+public class KeyStoreFactoryTest {
+
+    @DataProvider(name = "truststoreFormats")
+    public Object[][] truststoreFormats() {
+        return new Object[][]{
+                {JKS, "SUN"},
+                {PKCS12, "SunJSSE"},
+                {BCFKS, "BC"},
+        };
+    }
+
+    @Test(dataProvider = "truststoreFormats")
+    public void testCreateInstance(TruststoreFormat format, String provider) throws KeyStoreException {
+        KeyStore keyStore = KeyStoreFactory.createInstance(format);
+        assertThat(keyStore.getType()).isEqualTo(format.toString());
+        assertThat(keyStore.getProvider().getName()).isEqualTo(provider);
+    }
+}
