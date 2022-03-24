3c6af3098a3c84475c5325010b0a36f794275979
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2022-03-24 11:10:04 +0000

    Debug CI

--- src/it/non-unique-certificate-serial-numbers/bin/keytool_asserts.sh
+++ src/it/non-unique-certificate-serial-numbers/bin/keytool_asserts.sh
@@ -6,6 +6,7 @@ EXPECTED_OUTPUT=(
 )
 
 RESULT=$(keytool -list -keystore target/truststore.jks -storepass topsecret)
+echo "${RESULT}"
 
 for ENTRY in "${EXPECTED_OUTPUT[@]}"; do
   FOUND=$(echo "${RESULT}" | grep -q "${ENTRY}" && echo 1 || echo 0)
